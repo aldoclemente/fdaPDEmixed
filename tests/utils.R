45ee06b58aff47b53fbd50affb2a96c93691837e
aldoclemente <aldo.clemente@polimi.it>
2024-04-03 16:02:22 +0200

    mgcv vs fdaPDE ?

--- tests/utils.R
+++ tests/utils.R
@@ -32,7 +32,35 @@ contour.FEM <- function(x, limits=NULL, ...){
     colorbar(len = 1, title="")
 }
 
-sample_locations <- function(mesh, N){
+# sample_locations <- function(mesh, N){
+#   
+#   gridnum = 2 * floor( sqrt(N) )
+#   minX = min(mesh$nodes[,1])
+#   maxX = max(mesh$nodes[,1])
+#   minY = min(mesh$nodes[,2])
+#   maxY = max(mesh$nodes[,2])
+#   
+#   x = seq(minX+0.125, maxX-0.125, length.out = gridnum)
+#   y = seq(minY+0.125, maxY-0.125, length.out = gridnum)
+#   unif_grid <- expand.grid(x = x, y = y)
+#   idx <- fdaPDE:::CPP_search_points(mesh, as.matrix(unif_grid))
+#   locations <- as.matrix( unif_grid[which(idx != 0), ] )
+#   
+#   pts_list <- lapply(as.list(as.data.frame(t(locations))), sf::st_point)
+#   sf_pts <- sf::st_sfc(pts_list)
+#   
+#   pts_list <- rbind(mesh$nodes[as.logical(mesh$nodesmarkers),],
+#                     mesh$nodes[which(as.logical(mesh$nodesmarkers)==TRUE)[1],])
+#   sf_polygon <- sf::st_polygon(list(pts_list))
+#   
+#   idx <- sf::st_intersects(sf_pts, sf::st_boundary(sf_polygon), sparse = FALSE)
+#   if( length( which( idx == TRUE ) ) != 0 ){
+#     locations = locations[-which( idx == TRUE ), ]
+#   }
+#   return(locations[sample(1:nrow(locations), size=N),])
+# }
+
+sample_locations.mgcv <- function(mesh, N){
   
   gridnum = 2 * floor( sqrt(N) )
   minX = min(mesh$nodes[,1])
@@ -43,24 +71,15 @@ sample_locations <- function(mesh, N){
   x = seq(minX+0.125, maxX-0.125, length.out = gridnum)
   y = seq(minY+0.125, maxY-0.125, length.out = gridnum)
   unif_grid <- expand.grid(x = x, y = y)
-  idx <- fdaPDE:::CPP_search_points(mesh, as.matrix(unif_grid))
-  locations <- as.matrix( unif_grid[which(idx != 0), ] )
   
-  pts_list <- lapply(as.list(as.data.frame(t(locations))), sf::st_point)
-  sf_pts <- sf::st_sfc(pts_list)
+  idx <- mgcv::fs.test(unif_grid[,1],unif_grid[,2])
+  idx <- which(!is.na(idx))
   
-  pts_list <- rbind(mesh$nodes[as.logical(mesh$nodesmarkers),],
-                    mesh$nodes[which(as.logical(mesh$nodesmarkers)==TRUE)[1],])
-  sf_polygon <- sf::st_polygon(list(pts_list))
-  # plot(sf_polygon, col="red")  
-  # plot(sf_pts, col="black", pch=16, add=T)
-  # plot(sf::st_boundary(sf_polygon), col="blue", lwd=3, add = T)
-  
-  idx <- sf::st_intersects(sf_pts, sf::st_boundary(sf_polygon), sparse = FALSE)
-  if( length( which( idx == TRUE ) ) != 0 ){
-    locations = locations[-which( idx == TRUE ), ]
-  }
-  return(locations[sample(1:nrow(locations), size=N),])
+  locations <- as.matrix( unif_grid[idx, ] )
+  locations <- locations[sample(1:nrow(locations), size=N),]
+  rownames(locations) <- rep("", N)
+  colnames(locations) <- c("","")
+  return(locations)
 }
 
 compute_limits <- function(x){
@@ -91,8 +110,7 @@ plot.mesh.2D <- function(x, ...){
                  yend = x$nodes[x$edges[,2],2], 
                  color = I('black'), size = I(1),
                  showlegend = F) %>%
-    layout(scene = list(
-      aspectratio=list(x=1,y=1)), 
+    layout( 
       xaxis = list(
         title = '', showgrid = F, zeroline = F, showticklabels = F),
       yaxis = list(
